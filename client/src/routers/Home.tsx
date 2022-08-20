252ec5b5155f0280d9a2b3bfb8fe68de53ef6efc
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-20 16:31:40 +0900

    feat: Home 화면 생성

--- client/src/routers/Home.tsx
+++ client/src/routers/Home.tsx
@@ -1,4 +1,16 @@
+import styled from "styled-components";
+import Videos from "../components/Videos";
+
+const Wrapper = styled.div`
+  width: 1300px;
+  margin: 0 auto;
+`;
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Wrapper>
+      <Videos />
+    </Wrapper>
+  );
 }
 export default Home;
