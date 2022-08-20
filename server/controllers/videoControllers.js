8dafc8845ed337d78437e78848132aaf24c43712
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-20 12:19:19 +0900

    feat: Video 리스트 controller 생성

---
+++ server/controllers/videoControllers.js
@@ -0,0 +1,92 @@
+export const getVideos = () => {
+  const videos = [
+    {
+      id: 0,
+      title: "first movie",
+      description: "hello world",
+      hashtags: ["#moive", "hello", "world", "funny"],
+      mete: {
+        view: 15,
+        like: 42,
+        hate: 12,
+      },
+      owner: "jay",
+      comments: [
+        {
+          id: 0,
+          name: "big",
+          comment: "nice videos",
+        },
+        {
+          id: 1,
+          name: "big",
+          comment: "nice videos",
+        },
+        {
+          id: 2,
+          name: "big",
+          comment: "nice videos",
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: "second movie",
+      description: "hello world",
+      hashtags: ["#moive", "hello", "world", "funny"],
+      mete: {
+        view: 15,
+        like: 42,
+        hate: 12,
+      },
+      owner: "jay",
+      comments: [
+        {
+          id: 0,
+          name: "big",
+          comment: "nice videos",
+        },
+        {
+          id: 1,
+          name: "big",
+          comment: "nice videos",
+        },
+        {
+          id: 2,
+          name: "big",
+          comment: "nice videos",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "third movie",
+      description: "hello world",
+      hashtags: ["#moive", "hello", "world", "funny"],
+      mete: {
+        view: 15,
+        like: 42,
+        hate: 12,
+      },
+      owner: "jay",
+      comments: [
+        {
+          id: 0,
+          name: "big",
+          comment: "nice videos",
+        },
+        {
+          id: 1,
+          name: "big",
+          comment: "nice videos",
+        },
+        {
+          id: 2,
+          name: "big",
+          comment: "nice videos",
+        },
+      ],
+    },
+  ];
+  res.json(videos);
+};
