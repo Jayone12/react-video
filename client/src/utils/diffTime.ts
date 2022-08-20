cad2071093b8ad2498ef72d8f5a6f3e173d5bb32
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-20 16:13:57 +0900

    feat: 현재 시간으로부터 비디오 생성일 함수 생성

---
+++ client/src/utils/diffTime.ts
@@ -0,0 +1,29 @@
+const diffTime = (timestamp: number) => {
+  // 현재 시간
+  const currentTime = Date.now();
+
+  // 현재 시간에과 생성 시간의 차이
+  const diff = Math.floor((currentTime - timestamp) / 1000 / 60);
+
+  if (diff < 1) return `a few seconds`;
+  if (diff < 60) {
+    return `${diff} minutes ago`;
+  }
+
+  // 분으로 바꾼 diff에서 60으로 또 나누어 시간으로 바꾼다. (1시간은 60분)
+  const diffHour = Math.floor(diff / 60);
+  if (diffHour < 24) {
+    return `${diffHour} hours ago`;
+  }
+
+  // 시간으로 바꾼거에서 24로 나눠서 일수를 계산한다. (하루는 24시간)
+  const diffDay = Math.floor(diffHour / 24);
+  if (diffDay < 365) {
+    return `${diffDay} days ago`;
+  }
+
+  // 마지막은 년수로 계산
+  return `${Math.floor(diffDay / 365)} years ago`;
+};
+
+export default diffTime;
