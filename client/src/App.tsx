144962de93c683487ef54ca6e43012ff27d379c0
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-20 22:04:26 +0900

    feat: Header 컴포넌트 및 스타일 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./components/Header";
 import Home from "./routers/Home";
+
+const Wrapper = styled.div`
+  max-width: 1300px;
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Wrapper>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Wrapper>
   );
 }
 
