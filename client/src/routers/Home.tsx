2851fd6e1204c2816e84d7e2458e2bd92a17f7ed
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-20 22:06:00 +0900

    style: style 삭제

--- client/src/routers/Home.tsx
+++ client/src/routers/Home.tsx
@@ -1,16 +1,10 @@
-import styled from "styled-components";
 import Videos from "../components/Videos";
-
-const Wrapper = styled.div`
-  width: 1300px;
-  margin: 0 auto;
-`;
 
 function Home() {
   return (
-    <Wrapper>
+    <>
       <Videos />
-    </Wrapper>
+    </>
   );
 }
 export default Home;
