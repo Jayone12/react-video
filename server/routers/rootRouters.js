5c710aaa24a46db1f6ece30211be332d2a7eb373
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-22 15:48:20 +0900

    feat: 회원가입 router 생성

--- server/routers/rootRouters.js
+++ server/routers/rootRouters.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getLogin, postLogin } from "../controllers/userControllers";
+import {
+  getLogin,
+  postLogin,
+  getJoin,
+  postJoin,
+} from "../controllers/userControllers";
 
 const rootRouter = express.Router();
 
 rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").get(getJoin).post(postJoin);
 
 export default rootRouter;
