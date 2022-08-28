ff7df00d567267150ecd5d86bfabae108932a730
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-28 21:03:51 +0900

    feat: video upload form 전달 받아 DB생성

--- server/controllers/videoControllers.js
+++ server/controllers/videoControllers.js
@@ -1,102 +1,22 @@
-export const getVideos = (req, res) => {
-  const videos = [
-    {
-      id: 0,
-      title: "first movie",
-      description: "hello world",
-      hashtags: ["#moive", "hello", "world", "funny"],
-      createAt: 1660969408970,
-      thumbnail: "",
-      meta: {
-        view: 15,
-        like: 42,
-        hate: 12,
-      },
-      owner: "jay",
-      comments: [
-        {
-          id: 0,
-          name: "big",
-          comment: "nice videos",
-        },
-        {
-          id: 1,
-          name: "big",
-          comment: "nice videos",
-        },
-        {
-          id: 2,
-          name: "big",
-          comment: "nice videos",
-        },
-      ],
-    },
-    {
-      id: 1,
-      title: "second movie",
-      description: "hello world",
-      hashtags: ["#moive", "hello", "world", "funny"],
-      createAt: 1660969408970,
-      thumbnail: "",
-      meta: {
-        view: 15,
-        like: 42,
-        hate: 12,
-      },
-      owner: "jay",
-      comments: [
-        {
-          id: 0,
-          name: "big",
-          comment: "nice videos",
-        },
-        {
-          id: 1,
-          name: "big",
-          comment: "nice videos",
-        },
-        {
-          id: 2,
-          name: "big",
-          comment: "nice videos",
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: "third movie",
-      description: "hello world",
-      hashtags: ["#moive", "hello", "world", "funny"],
-      createAt: 1660969408970,
-      thumbnail: "",
-      meta: {
-        view: 15,
-        like: 42,
-        hate: 12,
-      },
-      owner: "jay",
-      comments: [
-        {
-          id: 0,
-          name: "big",
-          comment: "nice videos",
-        },
-        {
-          id: 1,
-          name: "big",
-          comment: "nice videos",
-        },
-        {
-          id: 2,
-          name: "big",
-          comment: "nice videos",
-        },
-      ],
-    },
-  ];
-  res.json(videos);
+import { now } from "mongoose";
+import Video from "../models/Video";
+
+export const getVideos = async (req, res) => {
+  const videos = await Video.find({});
+  return res.json(videos);
 };
 
-export const postUpload = (req, res) => {
-  console.log(req.body);
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description, hashtags },
+    files: { videoFile, thumbnail },
+  } = req;
+  await Video.create({
+    title,
+    videoUrl: videoFile[0].path,
+    description,
+    hashtags,
+    thumbnail: thumbnail[0].path,
+  });
+  return res.redirect("http://localhost:3000/");
 };
