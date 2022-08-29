035c57877cb68abf8e4cc5170b9e67ad0280c601
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-29 23:32:48 +0900

    feat: profile 이미지에 대한 multer middleware 생성

--- server/routers/rootRouters.js
+++ server/routers/rootRouters.js
@@ -10,13 +10,15 @@ import { postUpload } from "../controllers/videoControllers";
 
 const rootRouter = express.Router();
 const videoUpload = multer({ dest: "uploads/videos/" });
-const videoUpload = videoUpload.fields([
+const fileUpload = videoUpload.fields([
   { name: "videoFile", maxCount: 1 },
   { name: "thumbnail", maxCount: 1 },
 ]);
+const profileUpload = multer({ dest: "uploads/users/" });
+const profileImgFile = profileUpload.single("profileImg");
 
 rootRouter.route("/login").get(getLogin).post(postLogin);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/upload").post(videoUpload, postUpload);
+rootRouter.route("/join").get(getJoin).post(profileImgFile, postJoin);
+rootRouter.route("/upload").post(fileUpload, postUpload);
 
 export default rootRouter;
