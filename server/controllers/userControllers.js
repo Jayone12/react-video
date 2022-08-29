2cabaa960e3887b715c1ef4ede9fdde7817af231
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-29 23:37:53 +0900

    feat: Client에서 받은 내용으로 user DB 생성

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,13 +1,28 @@
+import User from "../models/User";
+
 export const getLogin = (req, res) => {
   res.send("login");
 };
-export const postLogin = (req, res) => {
+export const postLogin = async (req, res) => {
   console.log(req.body);
 };
 
 export const getJoin = (req, res) => {
   res.send("join");
 };
-export const postJoin = (req, res) => {
-  console.log(req.body);
+export const postJoin = async (req, res) => {
+  const {
+    body: { email, username, password, passwordConfirm },
+    file: { path },
+  } = req;
+  if (password !== passwordConfirm) {
+    return res.status(400);
+  }
+  await User.create({
+    email,
+    username,
+    password,
+    profileUrl: path,
+  });
+  return res.redirect("http://localhost:3000/login");
 };
